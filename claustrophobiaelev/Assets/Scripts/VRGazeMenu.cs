492ffe524981e9399b316ab2056c79e7b1cf2f39
MohamedLammaoui <45592592+MohamedLammaoui@users.noreply.github.com>
2020-05-05 12:04:15 +0200

    Man Menu added

---
+++ claustrophobiaelev/Assets/Scripts/VRGazeMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class VRGazeMenu : MonoBehaviour
+{
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+
+    void Update()
+    {
+        if(gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+
+        if(gvrTimer > totalTime)
+        {
+            Debug.Log("Invoked");
+            GVRClick.Invoke();
+        }
+    }
+
+    public void GvrOn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GvrOff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+    }
+}
